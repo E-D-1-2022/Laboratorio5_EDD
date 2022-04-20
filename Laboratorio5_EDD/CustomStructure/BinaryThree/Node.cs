f39d7b19be3e3ed77a1d4add1bcfe82ef4a85147
pcpisquiy <pcpisquiy@gmail.com>
2022-04-19 20:22:48 -0600

    Create of Custume structure

---
+++ Laboratorio5_EDD/CustomStructure/BinaryThree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStructure.BinaryTree
+{
+     public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public Node<T> Izquierda { get; set; }
+
+        public Node<T> Derecha { get; set; }
+
+    }
+}
---
+++ Laboratorio5_EDD/Laboratorio5_EDD/DTO/AutosDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace Laboratorio5_EDD.DTO
+{
+    public class AutosDTO
+    {
+        public int Placa { get; set; }
+        public Color color { get; set; }
+
+        public string Propietario { get; set; }
+
+        public decimal Latitud { get; set; }
+        public decimal Longitud { get; set; }
+    }
+}

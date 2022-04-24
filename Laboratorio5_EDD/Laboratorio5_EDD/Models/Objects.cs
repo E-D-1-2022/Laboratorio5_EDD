f35cbb9281b0dbeaa5761ed555bae5374d5b8316
Madydonis <98297522+Madydonis@users.noreply.github.com>
2022-04-24 15:15:18 -0600

    Update Objects.cs

--- Laboratorio5_EDD/Laboratorio5_EDD/Models/Objects.cs
+++ Laboratorio5_EDD/Laboratorio5_EDD/Models/Objects.cs
@@ -1,83 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace Laboratorio5_EDD.Models
+﻿
+using System.Drawing;
+public class Objects
 {
-    public class Vehiculo : Controller
+    public int placa { get; set; }
+
+    public Color color { get; set; }
+
+    public string propietario { get; set; }
+
+    public decimal latitud { get; }
+
+    public decimal longitud {get; }
+
+
+    public Objects(int _placa, Color _color, string _propietario, decimal _latitud, decimal _longitud)
     {
-        // GET: Vehiculo
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Vehiculo/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Vehiculo/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Vehiculo/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Vehiculo/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Vehiculo/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Vehiculo/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Vehiculo/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        placa = _placa;
+        color = _color;
+        propietario = _propietario;
+        latitud = _latitud;
+        longitud = _longitud;
     }
 }

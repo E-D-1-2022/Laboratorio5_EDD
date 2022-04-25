0df3080e2f28e1342b9c64c90d3908e334081690
pcpisquiy <pcpisquiy@gmail.com>
2022-04-24 18:11:01 -0600

    Subir solucionado a medias

--- Laboratorio5_EDD/Laboratorio5_EDD/Controllers/AutosController.cs
+++ Laboratorio5_EDD/Laboratorio5_EDD/Controllers/AutosController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Laboratorio5_EDD.DTO;
 using Laboratorio5_EDD.Entidad;
 using System.Drawing;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
 namespace Laboratorio5_EDD.Controllers
 {
     public class AutosController : Controller
@@ -86,6 +89,51 @@ namespace Laboratorio5_EDD.Controllers
         public IActionResult EliminarAuto(int placa) {
             AutosDTO auto = BaseDeDatos.ListaAutos.Find(new AutosDTO { Placa = placa });
             BaseDeDatos.ListaAutos.Remove(auto);
+            return View();
+        }
+        public IActionResult SubirArchivos() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CargarData(IFormFile file) {
+            if (file != null)
+            {
+                try
+                {
+                    //Sube archivo a carpeta temporal
+                    string ruta = Path.Combine(Path.GetTempPath(), file.Name);
+                    using (var stream = new FileStream(ruta, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    //Lee el archivo
+                    string allFileData = System.IO.File.ReadAllText(ruta);
+
+                    //Recorre archivo
+                    foreach (string lineaActual in allFileData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(lineaActual))
+                        {
+                            string[] info = lineaActual.Split(',');
+
+                            BaseDeDatos.ListaAutos.Add(new AutosDTO
+                            {
+                                Placa = Convert.ToInt32(info[0]),
+                                color = Color.FromName(info[1]),
+                                Propietario = info[2],
+                                Latitud = Convert.ToInt32(info[3]),
+                                Longitud = Convert.ToInt32(info[4]),
+                            });
+
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Error = ex.Message;
+                }
+            }
             return View();
         }
     }
